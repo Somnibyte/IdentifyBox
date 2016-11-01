8e33c395b0bd356ab17f194f65ed8d0bd8444003
castellanprime <okusanya.damilola@gmail.com>
2016-11-01 00:23:55 -0500

    Generated an empty Frame

---
+++ IdentifyBox/ImageProcessGUI.java
@@ -0,0 +1,14 @@
+/*
+*	File: ImageProcessGUI.java
+* 
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ImageProcessGUI extends Frame{
+
+	public ImageProcessGUI(){
+		super("Image Processing Application");
+	}
+}
