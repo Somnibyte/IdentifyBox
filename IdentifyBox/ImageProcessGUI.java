2f6bfba977f61e683291196b55c70acff259c470
castellanprime <okusanya.damilola@gmail.com>
2016-11-04 04:25:44 -0500

    TODO: Add a logger for exception handling

--- IdentifyBox/ImageProcessGUI.java
+++ IdentifyBox/ImageProcessGUI.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
+import javax.swing.JCheckBox;
 import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.BufferedImage;
@@ -14,16 +15,18 @@ import java.util.ArrayList;
 
 public class ImageProcessGUI extends JFrame{
 
-	public BitImage bitimage = new BitImage();
-	//public HashMap<String, String> actions = new HashMap<>();
 	private LoadImagePanel loadPanel;
+	private ImageTransformPanel transformPanel;
 
 	public ImageProcessGUI(){
 		super("Image Processing Application");
 		getContentPane().setLayout(new BorderLayout());
 
-		loadPanel = new LoadImagePanel("Original Image");
-		add(loadPanel, BorderLayout.LINE_START);
+		transformPanel = new ImageTransformPanel("Steps to generate output images");
+		loadPanel = new LoadImagePanel("Original Image", transformPanel);
+		
+		add(transformPanel, BorderLayout.LINE_END);
+		add(loadPanel, BorderLayout.CENTER);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
@@ -31,7 +34,12 @@ public class ImageProcessGUI extends JFrame{
 	}
 
 	public static void main(String[] args){
-		new ImageProcessGUI();
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ImageProcessGUI();
+            }
+        });
 	} 
 }
 
@@ -43,14 +51,14 @@ class LoadImagePanel extends JPanel{
 	protected String imagePath;
 	protected ImageUtils imageUtils = new ImageUtils();
 
-	public LoadImagePanel(String title){
+	public LoadImagePanel(String title, ImageTransformPanel transformPanel){
 		super();
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBorder(new TitledBorder(new EtchedBorder(), title));
 
 		labelImage = new JLabel("Image:");
 		imageLabel = new JLabel();
-		loadImage = new JButton("Load Image");
+		loadImage = new JButton("Load Image to be Compared Against");
 		loadImage.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				JFileChooser fc = new JFileChooser();
@@ -62,6 +70,7 @@ class LoadImagePanel extends JPanel{
 						BufferedImage img = ImageIO.read(file);
 						ArrayList<Integer> scaledDimens = imageUtils.getScaledDimensions(img, new Dimension(500, 500));
 						imageLabel.setIcon(new ImageIcon(imageUtils.resize(img, scaledDimens.get(0), scaledDimens.get(1))));
+						transformPanel.setImagePath(imagePath); 
 					} catch (IOException evt){
 						evt.printStackTrace();
 					}
@@ -74,17 +83,112 @@ class LoadImagePanel extends JPanel{
 		add(imageLabel);
 	}
 
-	public String getImageFileName(){
-		return imagePath;
-	}
 
 }
 
 
 class ImageTransformPanel extends JPanel{
 
-	public ImageTransformPanel(String title){
+	private String imagePath = " ";
+	private JButton loadImage;
+	private JCheckBox smoothBox, sharpenBox, histogramBox, edgeDetectBox, finalBox;
+	// private HashMap<String, Boolean> actions = new HashMap<>(); for automatic mode only
+	private BitImage bitimage = new BitImage();
+	private BufferedImage img;
+	private JButton applyButton;
+	private JPanel extras;
 
+	public ImageTransformPanel(String titled){
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(new TitledBorder(new EtchedBorder(), titled));
+
+		//JLabel instructions = new JLabel("You have to click \"Load Image to be Processed\" before you run any of the operations below.");
+
+		loadImage = new JButton("Load Image to be Processed");
+		loadImage.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent et){
+				int option = JOptionPane.showConfirmDialog(null, "Do you want to use a new image or keep using the old one?",
+								"Select Image for Processing", JOptionPane.YES_NO_OPTION);
+				if (option == JOptionPane.YES_OPTION){
+					JFileChooser fc = new JFileChooser();
+					int result = fc.showOpenDialog(null);
+					if (result == JFileChooser.APPROVE_OPTION){
+						File file = fc.getSelectedFile();
+						imagePath = file.getName();
+						img = null;
+					}
+				}
+			}
+		});
+
+		smoothBox = new JCheckBox("Apply smoothing");
+		histogramBox = new JCheckBox("Apply histogram equalization");
+
+		//setupActions();
+
+		smoothBox.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent st){
+				JCheckBox cb = (JCheckBox) st.getSource();
+        		if (cb.isSelected()) {
+        			try{
+        				// Will change the values of 4 to a more generic version.
+            			img = bitimage.applySmoothing((imagePath), 4, 4);
+            		} catch (IOException e){
+            			e.printStackTrace();
+            		}
+        		} 
+			}
+		});
+
+		histogramBox.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ht){
+				JCheckBox cb = (JCheckBox) ht.getSource();
+        		if (cb.isSelected()) {
+        			try{
+            			img = bitimage.applyHistogramEqualization((imagePath));
+        			}catch (IOException e){
+        				e.printStackTrace();
+        			}
+        		} 
+			}
+		});
+
+		applyButton = new JButton("Apply operation to image");
+		applyButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent at){
+				new OutputImageModal(img);
+			}
+		});
+
+
+		extras = new JPanel();
+		extras.setBorder(new TitledBorder(new EtchedBorder(), "Extra configs for option selected"));
+
+		add(loadImage);
+		add(smoothBox);
+		add(histogramBox);
+		add(extras);
+		add(applyButton);
+
+	}
+
+	/* For automatic mode only
+	private void setupActions(){
+		actions.put("Apply smoothing", false);
+		actions.put("Apply histogram equalization", false);
+	}*/
+
+	public void setImagePath(String imagePath){
+		this.imagePath = imagePath;
 	}
 }
 
+
+class OutputImageModal extends JDialog{
+
+	public OutputImageModal(BufferedImage img){
+
+	}
+
+}
