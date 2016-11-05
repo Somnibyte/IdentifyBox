166b4fce5eb93c9b0034c7d44a3f76f365fae877
castellanprime <okusanya.damilola@gmail.com>
2016-11-04 23:17:04 -0500

    TODO: Add kernel to GUI

--- IdentifyBox/ImageProcessGUI.java
+++ IdentifyBox/ImageProcessGUI.java
@@ -10,6 +10,7 @@ import javax.swing.border.*;
 import javax.swing.JCheckBox;
 import javax.imageio.ImageIO;
 import java.io.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
@@ -17,17 +18,18 @@ public class ImageProcessGUI extends JFrame{
 
 	private LoadImagePanel loadPanel;
 	private ImageTransformPanel transformPanel;
+	public String originalLoadedImageName = "";
 
 	public ImageProcessGUI(){
 		super("Image Processing Application");
 		getContentPane().setLayout(new BorderLayout());
 
-		transformPanel = new ImageTransformPanel("Steps to generate output images");
-		loadPanel = new LoadImagePanel("Original Image", transformPanel);
+		loadPanel = new LoadImagePanel("Original Image");
+		transformPanel = new ImageTransformPanel("Steps to generate output images", loadPanel);
 		
-		add(transformPanel, BorderLayout.LINE_END);
 		add(loadPanel, BorderLayout.CENTER);
-
+		add(transformPanel, BorderLayout.LINE_END);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
 		setVisible(true);
@@ -45,13 +47,13 @@ public class ImageProcessGUI extends JFrame{
 
 class LoadImagePanel extends JPanel{
 
-	protected JLabel labelImage;
-	protected JLabel imageLabel;
-	protected JButton loadImage;
-	protected String imagePath;
-	protected ImageUtils imageUtils = new ImageUtils();
+	private JLabel labelImage;
+	private JLabel imageLabel;
+	private JButton loadImage;
+	private ImageUtils imageUtils = new ImageUtils();
+	private String path = " ";
 
-	public LoadImagePanel(String title, ImageTransformPanel transformPanel){
+	public LoadImagePanel(String title){
 		super();
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBorder(new TitledBorder(new EtchedBorder(), title));
@@ -62,18 +64,17 @@ class LoadImagePanel extends JPanel{
 		loadImage.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				JFileChooser fc = new JFileChooser();
-				fc.setCurrentDirectory(new File("input/"));
+				fc.setCurrentDirectory(new File(new File("").getAbsolutePath(), "input"));
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("Bitmap Images", "bmp");
     			fc.setFileFilter(filter);
 				int result = fc.showOpenDialog(null);
 				if (result == JFileChooser.APPROVE_OPTION){
 					File file = fc.getSelectedFile();
-					imagePath = file.getName();
+					path = file.getAbsolutePath();
 					try{
 						BufferedImage img = ImageIO.read(file);
 						ArrayList<Integer> scaledDimens = imageUtils.getScaledDimensions(img, new Dimension(500, 500));
 						imageLabel.setIcon(new ImageIcon(imageUtils.resize(img, scaledDimens.get(0), scaledDimens.get(1))));
-						transformPanel.setImagePath(imagePath); 
 					} catch (IOException evt){
 						evt.printStackTrace();
 					}
@@ -86,6 +87,10 @@ class LoadImagePanel extends JPanel{
 		add(imageLabel);
 	}
 
+	
+	public String getPath(){
+		return path;
+	}
 
 }
 
@@ -101,7 +106,7 @@ class ImageTransformPanel extends JPanel{
 	private JButton applyButton;
 	private JPanel extras;
 
-	public ImageTransformPanel(String titled){
+	public ImageTransformPanel(String titled, LoadImagePanel loadPanel){
 		super();
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBorder(new TitledBorder(new EtchedBorder(), titled));
@@ -111,35 +116,40 @@ class ImageTransformPanel extends JPanel{
 		loadImage = new JButton("Load Image to be Processed");
 		loadImage.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent et){
-				int option = JOptionPane.showConfirmDialog(null, "Do you want to use a new image or keep using the old one?",
+				int option = JOptionPane.showConfirmDialog(null, "Do you want to use a new image(Yes to use a new image, No to use the loaded image)?",
 								"Select Image for Processing", JOptionPane.YES_NO_OPTION);
 				if (option == JOptionPane.YES_OPTION){
 					JFileChooser fc = new JFileChooser();
+					fc.setCurrentDirectory(new File(new File("").getAbsolutePath()));
 					int result = fc.showOpenDialog(null);
 					if (result == JFileChooser.APPROVE_OPTION){
 						File file = fc.getSelectedFile();
-						imagePath = file.getName();
-						img = null;
+						setImagePath(file.getAbsolutePath());
+						//img = null;
 					}
+				} else if (option == JOptionPane.NO_OPTION && !imagePath.isEmpty()){
+					setImagePath(loadPanel.getPath());
 				}
 			}
 		});
 
 		smoothBox = new JCheckBox("Apply smoothing");
 		histogramBox = new JCheckBox("Apply histogram equalization");
+		applyButton = new JButton("Apply operation to image");
 
 		//setupActions();
 
 		smoothBox.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent st){
-				JCheckBox cb = (JCheckBox) st.getSource();
-        		if (cb.isSelected()) {
+				JCheckBox bc = (JCheckBox) st.getSource();
+        		if (bc.isSelected()) {
         			try{
         				// Will change the values of 4 to a more generic version.
+        				if (imagePath.isEmpty()) System.out.println("Ficked");
             			img = bitimage.applySmoothing((imagePath), 4, 4);
             		} catch (IOException e){
             			e.printStackTrace();
-            		}imageLabel.setIcon(new ImageIcon
+            		}
         		} 
 			}
 		});
@@ -149,6 +159,7 @@ class ImageTransformPanel extends JPanel{
 				JCheckBox cb = (JCheckBox) ht.getSource();
         		if (cb.isSelected()) {
         			try{
+        				if (imagePath.isEmpty()) System.out.println("Ficked up");
             			img = bitimage.applyHistogramEqualization((imagePath));
         			}catch (IOException e){
         				e.printStackTrace();
@@ -157,10 +168,10 @@ class ImageTransformPanel extends JPanel{
 			}
 		});
 
-		applyButton = new JButton("Apply operation to image");
+		
 		applyButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent at){
-				new OutputImageModal(img, bitimage);
+				new OutputImageModal(img);
 			}
 		});
 
@@ -193,27 +204,35 @@ class OutputImageModal extends JDialog{
 	private JLabel outImgLabel;
 	private JButton saveButton, noSaveButton;
 	private ImageUtils imageUtils = new ImageUtils();
+	private JPanel imagePanel, commandPanel;
 
-	public OutputImageModal(BufferedImage img, BitImage bitimage){
-		outImgLabel = new JLable();
+	public OutputImageModal(BufferedImage img){
+
+		imagePanel = new JPanel(new BorderLayout());
+		commandPanel = new JPanel(new FlowLayout());
+
+		outImgLabel = new JLabel();
 		outImgLabel.setBorder(new TitledBorder(new EtchedBorder(), "Output Image"));
 
 		ArrayList<Integer> scaledDimens = imageUtils.getScaledDimensions(img, new Dimension(500, 500));
 		outImgLabel.setIcon(new ImageIcon(imageUtils.resize(img, scaledDimens.get(0), scaledDimens.get(1))));
 
 		saveButton = new JButton("Save");
+		noSaveButton = new JButton("Don't Save");
+
 		ActionListener lst = new ActionListener(){
 			public void actionPerformed(ActionEvent avt){
 				JFileChooser fileChooser = new JFileChooser();
 				fileChooser.setDialogTitle("Specify a file to save");
-				fileChooser.setCurrentDirectory(new File("output/"));
+				fileChooser.setCurrentDirectory(new File(new File("").getAbsolutePath(), "output"));
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("Bitmap Images", "bmp");
     			fileChooser.setFileFilter(filter);
 				int userSelection = fileChooser.showSaveDialog(null);
 				if (userSelection == JFileChooser.APPROVE_OPTION) {
  				    File file = fileChooser.getSelectedFile();
+ 				    //formatName = formatName + String
  				    try{
-     					bitimage.save(file.getName());
+     					ImageIO.write(img, "bmp", file);
      				} catch (IOException | RuntimeException ex){
      					ex.printStackTrace();
      				}
@@ -227,17 +246,26 @@ class OutputImageModal extends JDialog{
 			KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
 			JComponent.WHEN_IN_FOCUSED_WINDOW);
 
-		noSaveButton = new JButton("Don't Save");
+		
 		ActionListener ast = new ActionListener(){
 			public void actionPerformed(ActionEvent et){
 				dispose();
 			}
 		};
+		noSaveButton.addActionListener(ast);
 		getRootPane().registerKeyboardAction(ast,
 			KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
 			JComponent.WHEN_IN_FOCUSED_WINDOW);
 
+		imagePanel.add(outImgLabel, BorderLayout.PAGE_START);
 
+		commandPanel.add(saveButton);
+		commandPanel.add(noSaveButton);
+		imagePanel.add(commandPanel, BorderLayout.PAGE_END);
+
+		add(imagePanel);
+		pack();
+		setVisible(true);
 	}
 
 }
