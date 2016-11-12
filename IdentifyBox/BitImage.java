c760b86317a7dba7c863924d1e11e3d843623957
Guled <somnibyte@gmail.com>
2016-11-11 22:29:34 -0600

    Added back lost files

---
+++ IdentifyBox/BitImage.java
@@ -0,0 +1,528 @@
+/**
+ * BitImage.java
+ */
+
+/*
+Some Notes:
+
+BufferedImage class - Allows us to operate directly with image data (retrieving/setting pixel values)
+Raster - data structure that holds image data (allows us to scan image data)
+SampleModel - Interface that allows for pixel extraction.
+*/
+
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.Point;
+import java.awt.image.WritableRaster;
+import java.awt.image.SampleModel;
+import java.io.*;
+import javax.imageio.*;
+import java.util.Arrays;
+
+class BitImage{
+
+	private static SampleModel sampleModel;
+
+
+	public static void main(String[] args) throws IOException, RuntimeException{
+
+		// Files to play with
+		String input_file = "./input/im1-c.bmp";
+		String output_file_constrast = "constrast.bmp";
+		String outfile_file_kirschEdge = "smoothing_with_kirschEdge.bmp";
+
+/*
+		System.out.println("Reading an bitmap image" + input_file);
+		System.out.println("Printing the file in [0-255] range " + input_file);
+		//printPixelArray(getPixelArray(filename));
+		//***Histogram process***
+		System.out.println("Now applying contrast filter...");
+		writeToFile(writeToImage(constrast(getPixelArray(input_file), 3, 127)), output_file_constrast);
+		*/
+		//***Smoothing process***
+		System.out.println("Now applying kirsch filter...");
+		writeToFile(writeToImage(kirschEdge(smooth(getPixelArray(input_file), 6, 6))), outfile_file_kirschEdge);
+
+
+	}
+
+
+
+	/**
+	 * Private methods for testing
+	 *
+	 */
+  // Getting pixels from an array
+	private static int [][] getPixelArray(String filename) throws IOException{
+
+    	BufferedImage image = ImageIO.read(new File(filename));
+    	Raster raster = image.getData();
+    	sampleModel = raster.getSampleModel();
+
+    	int [][] array2D =  new int[image.getWidth()][image.getHeight()];
+
+		for (int j = 0; j < image.getWidth(); j++) {
+    		for (int k = 0; k < image.getHeight(); k++) {
+        		array2D[j][k] = raster.getSample(j, k, 0);
+    		}
+		}
+
+		return array2D;
+	}
+
+
+	// Modify test
+	private static int[][] modifyPixelArray(int [][] arr){
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+				if (arr[i][j] < 100){
+					arr[i][j] += 15;
+				}
+			}
+		}
+
+		return arr;
+	}
+
+
+
+
+	// Contrast Adjustment
+	private static int[][] constrast(int[][] arr, int contrast, int brightness){
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+				arr[i][j] = contrast*(arr[i][j] - brightness) + brightness;
+			}
+		}
+
+		return arr;
+	}
+
+
+	private static int[][] applyLapEdge(int[][] arr){
+		int sum = 0;
+		int avg;
+		//"frame" refers to small segment being modified at a time
+		int frameX = 3;
+		int frameY = 3;
+		int currentStartRow = 0;
+		int currentMaxRow = 3;
+		int currentStartColumn = 0;
+		int currentMaxColumn = 3;
+		int kernelColumn = 0;
+		int kernelRow = 0;
+		int [][] outputArray = new int[arr.length][arr[0].length];
+		int threshold = 10;
+		int [][] weightedKernel = new int[][] {
+			{-1, -1, -1},
+			{-1, 8, -1},
+			{-1, -1, -1}
+		};
+
+		//operate on rows
+		while(currentMaxRow < arr.length)
+		{
+			//System.out.println("currentStartRow = " + currentStartRow);
+			//operate on columns
+			while(currentMaxColumn < arr[0].length)
+			{
+				//System.out.println("currentStartColumn = " + currentStartColumn);
+				//gather average of frame
+				for(int i=currentStartRow; i<currentMaxRow; i++)
+				{
+					for(int j=currentStartColumn; j<currentMaxColumn; j++)
+					{
+						kernelColumn = j%3;
+						kernelRow = i%3;
+						sum += weightedKernel[kernelRow][kernelColumn]*arr[i][j];
+					}
+				}
+
+				if(sum > 255){
+					sum = 255;
+				}else if(sum < 0){
+					sum = 0;
+				}
+
+				if(sum <= threshold){
+					outputArray[currentStartRow+1][currentStartColumn+1] = 255;
+					sum = 0;
+				}else{
+					outputArray[currentStartRow+1][currentStartColumn+1] = 0;
+					sum = 0;
+				}
+
+				currentStartColumn += 1;//frameX;
+				currentMaxColumn += 1; //frameX;
+			}
+			currentStartRow += 1;//frameY;
+			currentMaxRow += 1;//frameY;
+			currentMaxColumn = 3;//frameX;
+			currentStartColumn = 0;
+			//System.out.println("adding to row");
+		}
+		return outputArray;
+	}
+
+	private static int[][] kirschEdge(int[][] arr) //whoops, its actually kirsch
+	{
+		int sum = 0;
+		int tempSumN = 0;
+		int tempSumS = 0;
+		int tempSumW = 0;
+		int tempSumE = 0;
+		int tempSumNE = 0;
+		int tempSumNW = 0;
+		int tempSumSW = 0;
+		int tempSumSE = 0;
+		int threshold = 350;
+		int avg;
+		//"frame" refers to small segment being modified at a time
+		int frameX = 3;
+		int frameY = 3;
+		int currentStartRow = 0;
+		int currentMaxRow = 3;
+		int currentStartColumn = 0;
+		int currentMaxColumn = 3;
+		int kernelColumn = 0;
+		int kernelRow = 0;
+		int [][] outputArray = new int[arr.length][arr[0].length];
+		int [][]  N = new int[][] {
+			{-3, -3, 5},
+			{-3, 0, 5},
+			{-3, -3, 5}
+		};
+		int [][]  NW = new int[][] {
+			{-3, -5, 5},
+			{-3, 0, 5},
+			{-3, -3, -3}
+		};
+		int [][]  W = new int[][] {
+			{5, 5, 5},
+			{-3, 0, -3},
+			{-3, -3, -3}
+		};
+		int [][]  SW = new int[][] {
+			{5, 5, -3},
+			{5, 0,-3},
+			{-3, -3, -3}
+		};
+		int [][]  S = new int[][] {
+			{5, -3, -3},
+			{5, 0, -3},
+			{5, -3, -3}
+		};
+		int [][]  SE = new int[][] {
+			{-3, -3, -3},
+			{5, 0, -3},
+			{5, 5, -3}
+		};
+		int [][]  E = new int[][] {
+			{-3, -3, -3},
+			{-3, 0, -3},
+			{5, 5, 5}
+		};
+		int [][]  NE = new int[][] {
+			{-3, -3, -3},
+			{-3, 0, 5},
+			{-3, 5, 5}
+		};
+
+		//operate on rows
+		while(currentMaxRow < arr.length)
+		{
+			//System.out.println("currentStartRow = " + currentStartRow);
+			//operate on columns
+			while(currentMaxColumn < arr[0].length)
+			{
+				//System.out.println("currentStartColumn = " + currentStartColumn);
+				//gather average of frame
+				for(int i=currentStartRow; i<currentMaxRow; i++)
+				{
+					for(int j=currentStartColumn; j<currentMaxColumn; j++)
+					{
+						kernelColumn = j%3;
+						kernelRow = i%3;
+
+						tempSumN += N[kernelRow][kernelColumn]*arr[i][j];
+						tempSumW += W[kernelRow][kernelColumn]*arr[i][j];
+						tempSumE += E[kernelRow][kernelColumn]*arr[i][j];
+						tempSumS += S[kernelRow][kernelColumn]*arr[i][j];
+
+						tempSumNE += NE[kernelRow][kernelColumn]*arr[i][j];
+						tempSumNW += NW[kernelRow][kernelColumn]*arr[i][j];
+						tempSumSE += SE[kernelRow][kernelColumn]*arr[i][j];
+						tempSumSW += SW[kernelRow][kernelColumn]*arr[i][j];
+
+					}
+				}
+				if(tempSumN > sum)
+					sum = tempSumN;
+				if(tempSumS > sum)
+					sum = tempSumS;
+				if(tempSumW > sum)
+					sum = tempSumW;
+				if(tempSumE > sum)
+					sum = tempSumE;
+				if(tempSumNE > sum)
+					sum = tempSumNE;
+				if(tempSumSW > sum)
+					sum = tempSumSW;
+				if(tempSumNW > sum)
+					sum = tempSumNW;
+				if(tempSumSE > sum)
+					sum = tempSumSE;
+
+				tempSumE = 0;
+				tempSumN = 0;
+				tempSumS = 0;
+				tempSumW = 0;
+				tempSumNE = 0;
+				tempSumNW = 0;
+				tempSumSE = 0;
+				tempSumSW = 0;
+
+				//System.out.println("sum variable is: " + sum);
+/*
+				if(sum > 255){
+					sum = 255;
+				}else if(sum < 0){
+					sum = 0;
+				}
+*/
+
+				if(sum <= threshold){
+					outputArray[currentStartRow+1][currentStartColumn+1] = 255;
+					sum = 0;
+				}else{
+					outputArray[currentStartRow+1][currentStartColumn+1] = 0;
+					sum = 0;
+				}
+
+				currentStartColumn += 1;//frameX;
+				currentMaxColumn += 1; //frameX;
+			}
+			currentStartRow += 1;//frameY;
+			currentMaxRow += 1;//frameY;
+			currentMaxColumn = 3;//frameX;
+			currentStartColumn = 0;
+			//System.out.println("adding to row");
+		}
+		return outputArray;
+	}
+
+	// Histogram Equalization
+	private static int[][] histogramEqualize(int [][] arr){
+
+		int[] H = new int[256];
+
+		// Initialize the Histogram
+		for(int i = 0; i < H.length; i++){
+			H[i] = 0;
+		}
+
+		// Compute the Histogram
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+					H[arr[i][j]] += 1;
+			}
+		}
+
+		// Compute the cumulative Histogram
+		for(int i = 1; i < 256; i++){
+			H[i] = H[i-1] + H[i];
+		}
+
+		// Normalize the cumulative Histogram
+		for(int i = 0; i < 256; i++){
+			H[i] = H[i] * 255 / (arr.length * arr[0].length);
+		}
+
+		//Modify Image
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+				arr[i][j] = H[arr[i][j]];
+			}
+		}
+
+		return arr;
+	}
+
+
+	/*
+	int [][] gaussianKernel = new int[][] {
+			{1, 4, 6, 4, 1},
+			{4, 16, 25, 16, 4},
+			{6, 28, 49, 28, 6},
+			{4, 16, 25, 16, 4},
+			{1, 4, 6, 4, 1}
+		};
+	*/
+	//Smoothing
+	//Smoothing
+	private static int[][] smooth(int[][] arr, int frameX, int frameY)
+	{
+		int sum = 0;
+		int median;
+		//"frame" refers to small segment being modified at a time
+		int currentStartRow = 0;
+		int currentMaxRow = frameY;
+		int currentStartColumn = 0;
+		int currentMaxColumn = frameX;
+		int kernelColumn = 0;
+		int kernelRow = 0;
+		int[] sortArray = new int[frameX*frameY];
+		int sortIndex=0;
+		int [][] outputArray = new int[arr.length][arr[0].length];
+		int[][] gaussianKernel = new int[][] {
+			{1, 4, 6 , 4, 1},
+			{3, 14, 23, 14, 3},
+			{6, 23, 49, 23, 6},
+			{3, 14, 23, 14, 3},
+			{1, 4, 6, 4, 1}
+		};
+		int [][] weightedKernel = new int[][] {
+			{3, 6, 3},
+			{6, 20, 6},
+			{3, 6, 3}
+		};
+
+		//operate on rows
+		while(currentMaxRow < arr.length)
+		{
+			//System.out.println("currentStartRow = " + currentStartRow);
+			//operate on columns
+			while(currentMaxColumn < arr[0].length)
+			{
+				//System.out.println("currentStartColumn = " + currentStartColumn);
+				//gather average of frame
+				for(int i=currentStartRow; i<currentMaxRow; i++)
+				{
+					for(int j=currentStartColumn; j<currentMaxColumn; j++)
+					{
+						kernelColumn = j%frameX;
+						kernelRow = i%frameY;
+						//sum += weightedKernel[kernelRow][kernelColumn]*arr[i][j];
+						sortArray[sortIndex] = arr[i][j];
+						sortIndex++;
+					}
+				}
+				sortIndex = 0;
+				Arrays.sort(sortArray);
+				sum = sortArray[(frameX*frameY)/2];
+				median = sum;
+				sum = 0;
+
+				if(median > 255)
+				{
+					System.out.println("Correcting to 255.");
+					median = 255;
+				}
+				else if(median < 0)
+				{
+					System.out.println("Correcting to 0.");
+					median = 0;
+				}
+				outputArray[currentStartRow+1][currentStartColumn+1] = median;
+
+				currentStartColumn += 1;
+				currentMaxColumn += 1;
+			}
+			currentStartRow += 1;
+			currentMaxRow += 1;
+			currentMaxColumn = frameX;
+			currentStartColumn = 0;
+			//System.out.println("adding to row");
+		}
+		return outputArray;
+	}
+
+  // Printing pixels
+	public static void printPixelArray(int [][] array2D){
+
+    	for (int x = 0; x < array2D.length; x++)
+    	{
+        	for (int y = 0; y < array2D[x].length; y++)
+        	{
+            	System.out.print(array2D[x][y] + " ");
+        	}
+        	System.out.println();
+    	}
+
+	}
+
+
+  // New Output Image
+	private static BufferedImage writeToImage(int [][] arr) throws IOException{
+
+		WritableRaster raster= Raster.createWritableRaster(sampleModel, new Point(0,0));
+    	for(int i=0;i<arr.length;i++)
+    	{
+        	for(int j=0;j<arr[i].length;j++)
+        	{
+            	raster.setSample(i,j,0,arr[i][j]);
+        	}
+    	}
+
+    	BufferedImage image=new BufferedImage(arr.length,arr[0].length,BufferedImage.TYPE_BYTE_GRAY);
+    	image.setData(raster);
+
+    	return image;
+	}
+
+
+	/**
+	 *	End of private methods for testing
+	 */
+
+
+	public static void writeToFile(BufferedImage image, String file_to_save) throws IOException, RuntimeException{
+		File file = new File(file_to_save);
+
+    	if (file.createNewFile()){
+    		System.out.println("File creation successful");
+    	} else {
+    		System.out.println("File already exists.");
+    	}
+
+		if (!ImageIO.write(image, "BMP", new File(file_to_save))) {
+  			throw new RuntimeException("Unexpected error writing image");
+		}
+	
+	}
+
+
+	/**
+	 * Start of public methods for GUI
+	 * 
+	 */
+	// Public methods
+
+	public BufferedImage applySmoothing(String filename, int width, int height) throws IOException{
+		return writeToImage(smooth(getPixelArray(filename), width, height));
+	}
+
+	public BufferedImage applyHistogramEqualization(String filename) throws IOException{
+		return writeToImage(histogramEqualize(getPixelArray(filename)));
+	}
+
+	public BufferedImage applyLapicianEdgeDetection(String filename) throws IOException{
+		System.out.println("Applied Lap Edge.");
+		return writeToImage(applyLapEdge(getPixelArray(filename)));
+	}
+
+	public BufferedImage applyKirschEdgeDetection(String filename) throws IOException{
+		return writeToImage(kirschEdge(getPixelArray(filename)));
+	}
+
+	public BufferedImage applyContrast(String filename, int constrast, int brightness) throws IOException{
+		return writeToImage(constrast(getPixelArray(filename), constrast, brightness));
+	}
+
+	/**
+	 *  End of public methods for GUI
+	 */
+
+
+
+}
---
+++ IdentifyBox/ImageProcessGUI.java
@@ -0,0 +1,455 @@
+/*
+*	File: ImageProcessGUI.java
+*
+*	Naming conventions:
+*	Buttons - (name)Btn
+*	Panels - (name)Panel
+*	Radio Button - (name)RdioBtn
+*	Labels - (name)Label
+* 	ButtonGroup - (name)BtnGrp
+*   Dialog - (name)diag
+*   ActionListener - (name)listner
+*
+*
+*	Components are added in the order they appear.
+* 
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+import javax.swing.JCheckBox;
+import javax.imageio.ImageIO;
+import java.io.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+
+/**
+ * 	This is the main GUI class
+ */
+public class ImageProcessGUI extends JFrame{
+
+	private LoadInitialImagePanel loadPanel;
+	private ImageTransformPanel transformPanel;
+
+	public ImageProcessGUI(){
+		super("Image Processing Application");
+		getContentPane().setLayout(new BorderLayout());
+
+		loadPanel = new LoadInitialImagePanel("Original Image");
+		transformPanel = new ImageTransformPanel("Steps to generate output images", loadPanel);
+		
+		add(loadPanel, BorderLayout.CENTER);			
+		add(transformPanel, BorderLayout.LINE_END);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(950, 750);
+		setVisible(true);
+	}
+
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ImageProcessGUI();
+            }
+        });
+	} 
+}
+
+
+/**
+ *  This acts the display for the initial image to be processed 
+ */
+class LoadInitialImagePanel extends JPanel{
+
+	private JLabel imgTitleLabel, emptyLabel;
+	private JButton loadImageBtn;
+
+	private ImageUtils imageUtils = new ImageUtils();
+	private String path = " ";
+
+	public LoadInitialImagePanel(String title){
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(new TitledBorder(new EtchedBorder(), title));
+
+		imgTitleLabel = new JLabel("Image:");
+		emptyLabel = new JLabel();
+		loadImageBtn = new JButton("Load Image to be Compared Against");
+		loadImageBtn.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				JFileChooser fc = new JFileChooser();
+				fc.setCurrentDirectory(new File(new File("").getAbsolutePath(), "input"));
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Bitmap Images", "bmp");
+    			fc.setFileFilter(filter);
+				int result = fc.showOpenDialog(null);
+				if (result == JFileChooser.APPROVE_OPTION){
+					File file = fc.getSelectedFile();
+					path = file.getAbsolutePath();
+					try{
+						BufferedImage img = ImageIO.read(file);
+						ArrayList<Integer> scaledDimens = imageUtils.getScaledDimensions(img, new Dimension(500, 500));
+						emptyLabel.setIcon(new ImageIcon(imageUtils.resize(img, scaledDimens.get(0), scaledDimens.get(1))));
+					} catch (IOException evt){
+						evt.printStackTrace();
+					}
+				}
+			}
+		});
+
+		add(loadImageBtn);
+		add(imgTitleLabel);
+		add(emptyLabel);
+		
+	}
+
+	
+	public String getPath(){
+		return path;
+	}
+
+}
+
+
+class ImageTransformPanel extends JPanel{
+
+	private JButton loadImageBtn, applyOperationBtn;
+	private JRadioButton smoothRdioBtn, contrastRdioBtn, histogramRdioBtn, kirschRdioBtn, lapicianRdioBtn;
+	private ButtonGroup imgProcessOperations;
+	private JLabel kernelLabel;
+	private JSpinner matOrderJSpinner;		// Input for order
+	private JPanel extrasPanel;
+
+	private String imagePath = " ";
+	private BufferedImage img;
+	private int order = 3;				// Since kernel is a order by order matrix
+	
+	private BitImage bitImage = new BitImage();
+	private KernelGrid kernelVals_GUI;
+
+	public ImageTransformPanel(String titled, LoadInitialImagePanel loadPanel){
+		super();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(new TitledBorder(new EtchedBorder(), titled));
+
+
+		loadImageBtn = new JButton("Load Image to be Processed");
+		smoothRdioBtn = new JRadioButton("Apply smoothing");
+		histogramRdioBtn = new JRadioButton("Apply histogram equalization");
+		kirschRdioBtn = new JRadioButton("Apply kirsch edge detection");
+		lapicianRdioBtn = new JRadioButton("Apply lapician edge detection");
+		contrastRdioBtn = new JRadioButton("Apply constrast");
+		applyOperationBtn = new JButton("Apply operation to image");
+
+		extrasPanel = new JPanel();			// holds all kernel related stuff
+		JPanel inputToKernelPanel = new JPanel();
+		kernelLabel = new JLabel("Enter n for an n by n kernel");
+
+
+		imgProcessOperations = new ButtonGroup ();
+		imgProcessOperations.add(smoothRdioBtn);
+		imgProcessOperations.add(histogramRdioBtn);
+		imgProcessOperations.add(lapicianRdioBtn);
+		imgProcessOperations.add(contrastRdioBtn);
+
+		smoothRdioBtn.setEnabled(false);
+		histogramRdioBtn.setEnabled(false);
+		kirschRdioBtn.setEnabled(false);
+		lapicianRdioBtn.setEnabled(false);
+		contrastRdioBtn.setEnabled(false);
+
+
+		loadImageBtn.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent et){
+				int option = JOptionPane.showConfirmDialog(null, "Do you want to use a new image(Yes to use a new image, No to use the last image loaded)?",
+								"Select Image for Processing", JOptionPane.YES_NO_OPTION);
+				if (option == JOptionPane.YES_OPTION){
+					JFileChooser fc = new JFileChooser();
+					fc.setCurrentDirectory(new File(new File("").getAbsolutePath()));
+					int result = fc.showOpenDialog(null);
+					if (result == JFileChooser.APPROVE_OPTION){
+						File file = fc.getSelectedFile();
+						setImagePath(file.getAbsolutePath());
+					}
+					// Loading the image from the LoadPanel
+				} else if (option == JOptionPane.NO_OPTION && " ".equals(imagePath)){ 
+					setImagePath(loadPanel.getPath());
+					System.out.println(imagePath);
+				}
+
+				smoothRdioBtn.setEnabled(true);
+				histogramRdioBtn.setEnabled(true);
+				kirschRdioBtn.setEnabled(true);
+				lapicianRdioBtn.setEnabled(true);
+				contrastRdioBtn.setEnabled(true);
+			}
+		});
+
+		// Will remove hardcoded values
+		ActionListener actListner = new ActionListener(){
+			@Override
+   			public void actionPerformed(ActionEvent e) {
+            	 if(e.getSource() == smoothRdioBtn){
+            	 	try{
+            	 		String options = JOptionPane.showInputDialog(null, "Enter your frameX and frameY values respectively.Example :4,5");
+            	 		if (options != null){
+	            	 		String[] values = options.split(",");
+	            	 		int frameX = Integer.parseInt(values[0]);
+	            	 		int frameY = Integer.parseInt(values[1]);
+	        				img = bitImage.applySmoothing((imagePath), frameX, frameY);
+	        			}
+        			} catch(IOException ie){
+        				ie.printStackTrace();
+        			}
+            	 }else if(e.getSource() == histogramRdioBtn){
+            	 	try{
+            	 		img = bitImage.applyHistogramEqualization(imagePath);
+        			} catch(IOException ie){
+        				ie.printStackTrace();
+   					}
+            	 } else if(e.getSource() == contrastRdioBtn){
+            	 	try{
+            	 		String options = JOptionPane.showInputDialog(null, "Enter your contrast and brightness values respectively.Example :4,5");
+            	 		if (options != null){
+	            	 		String[] values = options.split(",");
+	            	 		int contrast = Integer.parseInt(values[0]);
+	            	 		int brightness = Integer.parseInt(values[1]);
+            	 			img = bitImage.applyContrast((imagePath), contrast, brightness);
+            	 		}
+        			} catch(IOException ie){
+        				ie.printStackTrace();
+   					}
+   				 } else if(e.getSource() == kirschRdioBtn){
+            	 	try{
+            	 		img = bitImage.applyKirschEdgeDetection(imagePath);
+        			} catch(IOException ie){
+        				ie.printStackTrace();
+   					}
+            	 } else if(e.getSource() == lapicianRdioBtn){
+            	 	try{
+            	 		img = bitImage.applyLapicianEdgeDetection(imagePath);
+        			} catch(IOException ie){
+        				ie.printStackTrace();
+   					}
+            	 }
+            }
+        };
+
+		smoothRdioBtn.addActionListener(actListner);
+		histogramRdioBtn.addActionListener(actListner);
+		kirschRdioBtn.addActionListener(actListner);
+		lapicianRdioBtn.addActionListener(actListner);
+		contrastRdioBtn.addActionListener(actListner);
+
+		
+		extrasPanel.setLayout(new BoxLayout(extrasPanel, BoxLayout.PAGE_AXIS));
+		extrasPanel.setBorder(new TitledBorder(new EtchedBorder(), "Extra configs for option selected"));
+
+		
+		matOrderJSpinner = new JSpinner (new SpinnerNumberModel(
+			3,			// initial value
+			3,			// minimum value
+			12,			// maximum value
+			1			// step
+		));
+
+		kernelVals_GUI = new KernelGrid(order);
+
+		matOrderJSpinner.addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent e) {
+				JSpinner gf = (JSpinner)e.getSource();
+				order = (Integer)gf.getValue();
+				updateKernel(order);
+			}	
+		});
+
+		applyOperationBtn.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent at){	
+				new OutputImageModal(img, ImageTransformPanel.this);
+			}
+		});
+
+		
+		inputToKernelPanel.setLayout(new BoxLayout(inputToKernelPanel, BoxLayout.LINE_AXIS));
+		inputToKernelPanel.add(kernelLabel);
+		inputToKernelPanel.add(matOrderJSpinner);
+
+		extrasPanel.add(inputToKernelPanel);
+		extrasPanel.add(kernelVals_GUI);
+
+		add(loadImageBtn);
+		add(smoothRdioBtn);
+		add(histogramRdioBtn);
+		add(lapicianRdioBtn);
+		add(kirschRdioBtn);
+		add(contrastRdioBtn);
+		add(extrasPanel);
+		add(applyOperationBtn);
+
+	}
+
+	public void setImagePath(String imagePath){
+		this.imagePath = imagePath;
+	}
+
+	public void updateKernel(int order){
+		extrasPanel.remove(kernelVals_GUI);
+		extrasPanel.revalidate();
+		extrasPanel.repaint();
+		kernelVals_GUI = new KernelGrid(order);
+		extrasPanel.add(kernelVals_GUI);
+		extrasPanel.revalidate();
+		extrasPanel.repaint();
+	}
+
+	public JPanel getKernel(){
+		return kernelVals_GUI;
+	}
+}
+
+
+class KernelGrid extends JPanel{
+
+	private final ArrayList<JButton> list = new ArrayList<JButton>();
+	private int [][] data;
+	private int order;
+
+    private JButton getGridButton(final int row, final int column) {
+        int index = row * order + column;
+        return list.get(index);
+    }
+
+	private JButton createGridButton(final int row, final int col, int value) {
+        JButton b = new JButton(String.valueOf(value));
+        b.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	JButton gb = KernelGrid.this.getGridButton(row, col);
+                String val = JOptionPane.showInputDialog(null, "Enter your value for this cell");
+                if (val != null){
+                	gb.setText(val);
+                	data[row][col] = Integer.parseInt(val);
+                }
+                printKernelCells();
+            }
+        });
+        return b;
+    }
+
+    private void resetData(int [][] data){
+    	for (int i =0; i < data.length; i++){
+    		for (int j = 0; j < data[0].length; j++){
+    			data[i][j] = 0;
+    		}
+    	}
+    }
+
+    public KernelGrid(int order){
+    	this.order = order;
+    	data = new int[order][order];
+    	resetData(data);
+    	setLayout(new GridLayout(order, order));
+        for (int i = 0; i < order * order; i++) {
+            int row = i / order;
+            int col = i % order;
+            JButton gb = createGridButton(row, col, 0);
+            list.add(gb);
+            add(gb);
+        }
+	}
+
+	public int [][] getValueData(){
+		return data;
+	}
+
+	public void printKernelCells(){
+		StringBuilder sb = new StringBuilder();
+		for (int i =0; i < data.length; i++){
+    		for (int j = 0; j < data[0].length; j++){
+    			sb.append(data[i][j] + " ");
+    		}
+    		sb.append("\n");
+    	}
+    	System.out.println(sb.toString());
+	}
+
+}
+
+
+class OutputImageModal extends JDialog{
+
+	private JLabel outImgLabel;
+	private JButton saveBtn, noSaveBtn;
+	private ImageUtils imageUtils = new ImageUtils();
+	private JPanel imagePanel, commandPanel;
+
+	public OutputImageModal(BufferedImage img, ImageTransformPanel transformPanel){
+
+		imagePanel = new JPanel(new BorderLayout());
+		commandPanel = new JPanel(new FlowLayout());
+		saveBtn = new JButton("Save");
+		noSaveBtn = new JButton("Don't Save");
+
+		outImgLabel = new JLabel();
+		outImgLabel.setBorder(new TitledBorder(new EtchedBorder(), "Output Image"));
+
+		ArrayList<Integer> scaledDimens = imageUtils.getScaledDimensions(img, new Dimension(500, 500));
+		outImgLabel.setIcon(new ImageIcon(imageUtils.resize(img, scaledDimens.get(0), scaledDimens.get(1))));
+
+		
+		ActionListener saveListner = new ActionListener(){
+			public void actionPerformed(ActionEvent avt){
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Specify a file to save");
+				fileChooser.setCurrentDirectory(new File(new File("").getAbsolutePath(), "output"));
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Bitmap Images", "bmp");
+    			fileChooser.setFileFilter(filter);
+				int userSelection = fileChooser.showSaveDialog(null);
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+ 				    File file = fileChooser.getSelectedFile();
+ 				    transformPanel.setImagePath(file.getAbsolutePath());
+ 				    try{
+     					ImageIO.write(img, "bmp", file);
+     				} catch (IOException | RuntimeException ex){
+     					ex.printStackTrace();
+     				}
+				}
+				dispose();
+			}
+		};
+
+		saveBtn.addActionListener(saveListner);
+		getRootPane().setDefaultButton(saveBtn);
+		getRootPane().registerKeyboardAction(saveListner,
+			KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
+			JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+		
+		ActionListener exitListner = new ActionListener(){
+			public void actionPerformed(ActionEvent et){
+				dispose();
+			}
+		};
+		noSaveBtn.addActionListener(exitListner );
+		getRootPane().registerKeyboardAction(exitListner ,
+			KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+			JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+		imagePanel.add(outImgLabel, BorderLayout.PAGE_START);
+
+		commandPanel.add(saveBtn);
+		commandPanel.add(noSaveBtn);
+		imagePanel.add(commandPanel, BorderLayout.PAGE_END);
+
+		add(imagePanel);
+		pack();
+		setVisible(true);
+	}
+
+}
